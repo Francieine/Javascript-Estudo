1cc206ff7d7ee2c6a0e897d632ece7dca829b5ae
Francieine Soares Sabino <73969123+Francieine@users.noreply.github.com>
2023-08-10 08:51:56 -0300

    Create index.js

---
+++ Javascript/Colorify-Project/index.js
@@ -0,0 +1,10 @@
+function paint(color) {
+    const circle = document.getElementById('circleID');
+    circle.style = `background-color:${color}`;
+}
+
+function paintBody(color2){
+    const bodyContainer = document.getElementById('bodyID');
+    bodyContainer.style = `background-color:${color2}`;
+}
+
